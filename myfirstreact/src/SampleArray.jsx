c6bd8022fca3807ed21adbf866b132b6205538fa
adhithya-bolishetti <adhithya.bolishetti@gmail.com>
2026-06-16 12:15:53 +0530

    feat: Implement user data display in SampleArray component

--- myfirstreact/src/SampleArray.jsx
+++ myfirstreact/src/SampleArray.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { userData } from './data'
 
 let array = ["Apple", "Mango", 24, {userName:"adhithya-bolishetti"}]
 
@@ -10,18 +11,22 @@ let players = [
         hockey:"Dyan chand"
     }
 ]
+
+console.log(userData)
 const SampleArray = () => {
   return (
     <div>
-        {players.map((item) => {
-            return(
-                <div>
-                    {item.cricket}
-                </div>
-            )
-        })}
-        {players[0].chess}
-        {players[0].hockey}
+        {
+            userData.map((user) => {
+                return(
+                    <div style={{border: "1px solid black"}}>
+                        <div>{user.username}</div>
+                        <div>{user.email}</div>
+                        <div>{user.address.street}</div>
+                    </div>
+                )
+            }
+        )}
     </div>
   )
 }
