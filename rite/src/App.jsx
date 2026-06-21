c20572f65a85f61cbdc8564249e64d40568ec91c
adhithya-bolishetti <adhithya.bolishetti@gmail.com>
2026-06-21 15:40:13 +0530

    feat: add routing with FirstPage, SecondPage, ThirdPage, and FourthPage components

--- rite/src/App.jsx
+++ rite/src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import FirstPage from './routing/FirstPage'
+import SecondPage from './routing/SecondPage'
+import ThirdPage from './routing/ThirdPage'
+import FourthPage from './routing/FourthPage'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path='/signup' element = {<FirstPage />} />
+        <Route path='/login' element = {<SecondPage />} />
+      </Routes>
+      {/* <FirstPage/>
+      <SecondPage />
+      <ThirdPage />
+      <FourthPage /> */}
+    </div>
   )
 }
 
